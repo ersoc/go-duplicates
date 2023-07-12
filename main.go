75e6a3bcf48453d777a2a6bd489706235ec2715b
ersoc <122680687+ersoc@users.noreply.github.com>
2023-07-12 23:50:14 +0200

    add basic poc

---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"flag"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+type FileHash struct {
+	Name string `json:"name"`
+	Path string `json:"path"`
+}
+
+func startServer() {}
+
+func main() {
+	path := flag.String("p", ".", "path to the directory")
+	flag.Parse()
+
+	hashes, err := computeHashes(*path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = writeHashesToJson(hashes, "hashes.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func computeHashes(root string) (map[string][]FileHash, error) {
+	hashes := make(map[string][]FileHash)
+
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			hash, err := hashFile(path)
+			if err != nil {
+				return err
+			}
+
+			hashes[hash] = append(hashes[hash], FileHash{
+				Name: info.Name(),
+				Path: path,
+			})
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return hashes, nil
+}
+
+func hashFile(path string) (string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(hash.Sum(nil)), nil
+}
+
+func writeHashesToJson(hashes map[string][]FileHash, filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+
+	return encoder.Encode(hashes)
+}
