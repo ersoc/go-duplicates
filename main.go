18e05adc06b6c1a43217291d6048efd52faf8d7c
stuckwithjs <37895882+stuckwithjs@users.noreply.github.com>
2023-08-13 18:31:42 +0200

    add some logic

--- main.go
+++ main.go
@@ -1,92 +1,31 @@
 package main
 
 import (
-	"crypto/md5"
-	"encoding/hex"
-	"encoding/json"
-	"flag"
-	"io"
-	"log"
 	"os"
-	"path/filepath"
+	"time"
 )
 
-type FileHash struct {
-	Name string `json:"name"`
-	Path string `json:"path"`
+type Metadata struct {
+	Name    string
+	Size    int64
+	ModTime time.Time
 }
 
-func startServer() {}
-
-func main() {
-	path := flag.String("p", ".", "path to the directory")
-	flag.Parse()
-
-	hashes, err := computeHashes(*path)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = writeHashesToJson(hashes, "hashes.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func computeHashes(root string) (map[string][]FileHash, error) {
-	hashes := make(map[string][]FileHash)
-
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if !info.IsDir() {
-			hash, err := hashFile(path)
-			if err != nil {
-				return err
-			}
-
-			hashes[hash] = append(hashes[hash], FileHash{
-				Name: info.Name(),
-				Path: path,
-			})
-		}
-
-		return nil
-	})
-
+func GenerateMetadata(path string) (*Metadata, error) {
+	fileInfo, err := os.Stat(path)
 	if err != nil {
 		return nil, err
 	}
 
-	return hashes, nil
+	metadata := &Metadata{
+		Name:    fileInfo.Name(),
+		Size:    fileInfo.Size(),
+		ModTime: fileInfo.ModTime(),
+	}
+
+	return metadata, nil
 }
 
-func hashFile(path string) (string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return "", err
-	}
-	defer file.Close()
+func main() {
 
-	hash := md5.New()
-	if _, err := io.Copy(hash, file); err != nil {
-		return "", err
-	}
-
-	return hex.EncodeToString(hash.Sum(nil)), nil
-}
-
-func writeHashesToJson(hashes map[string][]FileHash, filename string) error {
-	file, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	encoder := json.NewEncoder(file)
-	encoder.SetIndent("", "  ")
-
-	return encoder.Encode(hashes)
 }
